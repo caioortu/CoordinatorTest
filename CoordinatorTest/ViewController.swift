db89a79b09b36d381dd6274d1bb9abc353eeb2f5
Caio Ortu <caio.ortu@avenuecode.com>
2021-01-26 18:34:11 -0300

    Initial Commit

---
+++ CoordinatorTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorTest
+//
+//  Created by Caio Ortu on 1/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
